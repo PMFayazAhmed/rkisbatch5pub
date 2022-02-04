b1fd16dcb93eafb58a12da34b2d0edb56edc5f2c
PMFayazAhmed <91198298+PMFayazAhmed@users.noreply.github.com>
2022-02-04 20:09:50 +0530

    Update Sum.java

--- Sum.java
+++ Sum.java
@@ -8,7 +8,7 @@ Scanner s = new Scanner(System.in);
 num1=s.nextInt();
 Sysstem.out.println("Enter second number value");
 num2=s.nextInt();
-res=num1+num2;
+res=(num1+num2)/num2;
 System.out.println("This program is to find sum of two integer values");
 System.out.println("Result is "+res);
 if(res>0)
